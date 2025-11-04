9d0dc491b6916e7bc42bb8dae0ecaefa922e1cfb
Luis Arce <luilliarcec@gmail.com>
2025-11-04 14:41:22 -0500

    Add Prometheus configuration and service provider for monitoring setup

---
+++ app/Providers/PrometheusServiceProvider.php
@@ -0,0 +1,39 @@
+<?php
+
+namespace App\Providers;
+
+use Illuminate\Support\ServiceProvider;
+use Spatie\Prometheus\Collectors\Horizon\CurrentMasterSupervisorCollector;
+use Spatie\Prometheus\Collectors\Horizon\CurrentProcessesPerQueueCollector;
+use Spatie\Prometheus\Collectors\Horizon\CurrentWorkloadCollector;
+use Spatie\Prometheus\Collectors\Horizon\FailedJobsPerHourCollector;
+use Spatie\Prometheus\Collectors\Horizon\HorizonStatusCollector;
+use Spatie\Prometheus\Collectors\Horizon\JobsPerMinuteCollector;
+use Spatie\Prometheus\Collectors\Horizon\RecentJobsCollector;
+use Spatie\Prometheus\Collectors\Queue\QueueDelayedJobsCollector;
+use Spatie\Prometheus\Collectors\Queue\QueueOldestPendingJobCollector;
+use Spatie\Prometheus\Collectors\Queue\QueuePendingJobsCollector;
+use Spatie\Prometheus\Collectors\Queue\QueueReservedJobsCollector;
+use Spatie\Prometheus\Collectors\Queue\QueueSizeCollector;
+use Spatie\Prometheus\Facades\Prometheus;
+
+class PrometheusServiceProvider extends ServiceProvider
+{
+    public function register(): void
+    {
+        // $this->registerQueueCollectors(['default']);
+    }
+
+    public function registerQueueCollectors(array $queues = [], ?string $connection = null): self
+    {
+        Prometheus::registerCollectorClasses([
+            QueueSizeCollector::class,
+            QueuePendingJobsCollector::class,
+            QueueDelayedJobsCollector::class,
+            QueueReservedJobsCollector::class,
+            QueueOldestPendingJobCollector::class,
+        ], compact('connection', 'queues'));
+
+        return $this;
+    }
+}
